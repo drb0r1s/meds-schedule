49dcfb2f3756f5e418097e750b17bd90728b5fe6
drb0r1s <drb0r1sdev@gmail.com>
2025-04-26 18:14:21 +0200

    Port update

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const DB = require("./db/index.js");
 const app = express();
 const port = 9999;
 
-app.listen(port, () => console.log("Listening on http://localhost:9998"));
+app.listen(port, () => console.log(`Server is listening on http://88.200.63.148:9998/`));
 
 app.get("/", (req, res) => {
     res.send("Hello world!");
