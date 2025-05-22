34d453c7e9f648edb45c55d5574f0d57c3f32473
drb0r1s <drb0r1sdev@gmail.com>
2025-05-22 16:17:23 +0200

    Frontend: Checkbox component update (onCheck)

--- frontend/src/components/checkbox/Checkbox.jsx
+++ frontend/src/components/checkbox/Checkbox.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./Checkbox.css";
 import { images } from "../../data/images";
 
-const Checkbox = ({ title, value, setValue }) => {
+const Checkbox = ({ title, value, setValue, onCheck }) => {
     return(
-        <div className="checkbox-holder" onClick={() => setValue(!value)}>
+        <div className="checkbox-holder" onClick={onCheck ? onCheck : () => setValue(!value)}>
             <div className={`checkbox ${value && "checkbox-active"}`}>
                 {value && <img src={images.checkIcon} alt="CHECK" />}
             </div>
